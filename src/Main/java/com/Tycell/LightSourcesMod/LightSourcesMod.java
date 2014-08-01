dfa6dd447869b5eca0038e8d4459ee040ebdfef5
Tycell <tycell145@gmail.com>
2014-08-01 18:36:07 +0100

    2nd sync
    still not v1

---
+++ src/Main/java/com/Tycell/LightSourcesMod/LightSourcesMod.java
@@ -0,0 +1,14 @@
+package com.Tycell.LightSourcesMod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="LightSourcesMod", name="LightSources", version="1.7.1-1.0")
+public class LightSourcesMod
+{
+    @Mod.EventHandler
+    public void preInit (FMLPreInitializationEvent event)
+    {
+
+    }
+}
