51562a0165800def20b14cfc16026c43853361d7
HotelCalifornia <ak.brooke@sbcglobal.net>
2014-08-02 19:16:12 -0500

    use lowercase for package names

--- src/Main/java/com/Tycell/LightSourcesMod/LightSourcesMod.java
+++ src/Main/java/com/Tycell/LightSourcesMod/LightSourcesMod.java
@@ -1,9 +1,9 @@
-package com.Tycell.LightSourcesMod;
+package com.tycell.lightsourcesmod;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid="LightSourcesMod", name="LightSources", version="1.7.1-1.0")
+@Mod(modid="lightsourcesmod", name="LightSources", version="1.7.1-1.0")
 public class LightSourcesMod
 {
     @Mod.EventHandler
